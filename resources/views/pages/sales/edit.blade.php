931101eb8378bd7785d7003fe033f3b8993bf38e
Dieng221 <ibrahima.dieng150@gmail.com>
2024-12-30 05:06:29 +0000

    30/12/2024 05:06

--- resources/views/pages/sales/edit.blade.php
+++ resources/views/pages/sales/edit.blade.php
@@ -1,5 +1,75 @@
 @extends('components.layout')
 
 @section('content')
-   <div>Edit sale</div>
+   
+   <div class="container-fluid">
+    <div class="row">
+       <div class="col-lg-12">
+
+          <div class="breadcrumb-main">
+             <h4 class="text-capitalize breadcrumb-title"></h4>
+             <div class="breadcrumb-action justify-content-center flex-wrap">
+                <nav aria-label="breadcrumb">
+                   <ol class="breadcrumb">
+                      <li class="breadcrumb-item"><a href="#"><i class="uil uil-estate"></i>Home</a></li>
+                      <li class="breadcrumb-item">
+                      <a href="#"><i class="uil uil-estate"></i>Administrateurs</a>
+
+                      </li>
+                      <li class="breadcrumb-item active" aria-current="page">Création</li>
+                   </ol>
+                </nav>
+             </div>
+          </div>
+
+       </div>
+    </div>
+    <div class="row">
+       <div class="col-lg-12">
+          <div class="card card-Vertical card-default card-md mb-4">
+             <div class="card-header">
+                <h6>Modifier une vente </h6>
+             </div>
+             <div class="card-body py-md-30">
+                <form method="POST" action="">
+                    @csrf
+                   <div class="row">
+                      <div class="col-md-6 mb-25">
+                         <input type="text" name="nom" class="form-control ih-medium ip-gray radius-xs b-light px-15" placeholder="Nom">
+                      </div>
+                      <div class="col-md-6 mb-25">
+                         <input type="text" name="prenom"class="form-control ih-medium ip-gray radius-xs b-light px-15" placeholder="Prenom">
+                      </div>
+                      <div class="col-md-6 mb-25">
+                         <input type="email" name="email" class="form-control ih-medium ip-gray radius-xs b-light px-15" placeholder="Adresse mail">
+                      </div>
+                      <div class="col-md-6 mb-25">
+                         <input type="text" name="numero" class="form-control ih-medium ip-gray radius-xs b-light px-15" placeholder="Numéro">
+                      </div>
+                      <div class="col-md-6 mb-25">
+                        <input type="password" name="password" class="form-control ih-medium ip-gray radius-xs b-light px-15" placeholder="Mot de passe">
+                     </div>
+                      <div class="col-md-6 mb-25">
+                        <select class="form-control ih-medium ip-gray radius-xs b-light px-15" name="module">
+                            <option selected>Module...</option>
+                            <option value="Bim Bara">Bim Bara</option>
+                            <option value="Bim Livraison">Bim Livraison</option>
+                            <option value="Bim Dabali">Bim Dabali</option>
+                            <option value="Bim Market-Event">Bim Market-Event</option>
+                        </select>
+                    </div>
+                      <div class="col-md-6">
+                         <div class="layout-button mt-0">
+                            <button type="submit" class="btn btn-default btn-squared btn-light px-20 ">Annuler</button>
+                            <button type="submit" class="btn btn-primary btn-default btn-squared px-30">Créer</button>
+                         </div>
+                      </div>
+                   </div>
+                </form>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>
+
 @endsection
--- resources/views/pages/sales/show.blade.php
+++ resources/views/pages/sales/show.blade.php
@@ -1,5 +1,111 @@
 @extends('components.layout')
 
 @section('content')
-   <div>Show sale</div>
+   
+   <div class="container-fluid">
+    <div class="row">
+       <div class="col-lg-12">
+
+          <div class="breadcrumb-main">
+             <h4 class="text-capitalize breadcrumb-title"></h4>
+             <div class="breadcrumb-action justify-content-center flex-wrap">
+                <nav aria-label="breadcrumb">
+                   <ol class="breadcrumb">
+                      <li class="breadcrumb-item"><a href="#"><i class="uil uil-estate"></i>Accueil</a></li>
+                      <li class="breadcrumb-item"><a href="#">Entreprise</a></li>
+                      <li class="breadcrumb-item active" aria-current="page">10</li>
+                   </ol>
+                </nav>
+             </div>
+          </div>
+
+       </div>
+    </div>
+    <div class="row">
+       <div class="col-lg-12">
+          <div class="card card-Vertical card-default card-md mb-4">
+             <div class="card-header">
+                <h6>Détails de la vente </h6>
+             </div>
+             <div class="card-body py-md-30">
+             <div class="container mt-4">
+  <div class="row">
+    <div class="col-md-8 offset-md-2">
+      <div class="card">
+        <div class="card-body">
+          <div class="row mb-3">
+            <div class="col-md-4">
+              <h5 class="card-title">Nom:</h5>
+            </div>
+            <div class="col-md-8">
+              <p class="card-text">DC-Knowing</p>
+            </div>
+          </div>
+          <div class="row mb-3">
+            <div class="col-md-4">
+              <h5 class="card-title">Secteur d'activité:</h5>
+            </div>
+            <div class="col-md-8">
+              <p class="card-text">Comptabilité</p>
+            </div>
+          </div>
+          <div class="row mb-3">
+            <div class="col-md-4">
+              <h5 class="card-title">Localisation:</h5>
+            </div>
+            <div class="col-md-8">
+              <p class="card-text">Abidjan, Cocody</p>
+            </div>
+          </div>
+          <div class="row mb-3">
+            <div class="col-md-4">
+              <h5 class="card-title">Email:</h5>
+            </div>
+            <div class="col-md-8">
+              <p class="card-text">dc-knowing@gmail.com</p>
+            </div>
+          </div>
+          <div class="row mb-3">
+            <div class="col-md-4">
+              <h5 class="card-title">Contact:</h5>
+            </div>
+            <div class="col-md-8">
+              <p class="card-text">+225 0778087378</p>
+            </div>
+          </div>
+          <div class="row mb-3">
+            <div class="col-md-4">
+              <h5 class="card-title">Statut:</h5>
+            </div>
+            <div class="col-md-8">
+                  <p class="card-text">Actif</p>
+            </div>
+
+          </div>
+          <div class="row mb-3">
+            <div class="col-md-4">
+              <h5 class="card-title">Date d'Enregistrement:</h5>
+            </div>
+            <div class="col-md-8">
+              <p class="card-text">30/12/2024</p>
+            </div>
+          </div>
+          <div class="d-flex text-center mt-4">
+            <a href="" class="btn btn-outline-secondary me-2">Retour</a>
+            <a href="" class="btn btn-outline-primary me-2">Modifier</a>
+            <a href="" class="btn btn-outline-danger">Supprimer</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+             </div>
+          </div>
+          <!-- ends: .card -->
+
+       </div>
+    </div>
+ </div>
+
 @endsection
