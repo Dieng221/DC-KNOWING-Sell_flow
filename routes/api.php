7f5703ce30cbac1c5ef805cf90933d3c94b4aea1
Mrsig11 <sis.konan07@gmail.com>
2025-01-05 16:41:07 +0000

    MAJ

--- routes/api.php
+++ routes/api.php
@@ -23,43 +23,35 @@ Route::group([
     // 'middleware' => 'auth:sanctum'
 ], function () {
     Route::group(['prefix' => 'sales'], function(){
-        // Route::get('/list', [SaleController::class, 'index'])->name('sales.list');
-        // Route::get('/create', [SaleController::class, 'create'])->name('sales.create');
+        Route::get('/list', [SaleController::class, 'indexAPI'])->name('sales.list');
         Route::post('/store', [SaleController::class, 'storeAPI'])->name('sales.store');
-        // Route::get('/show/{id}', [SaleController::class, 'show'])->name('sales.show');
-        // Route::get('/edit/{id}', [SaleController::class, 'edit'])->name('sales.edit');
-        // Route::post('/update/{id}', [SaleController::class, 'edit'])->name('sales.update');
-        // Route::get('/delete/{id}', [SaleController::class, 'destroy'])->name('sales.delete');
+        Route::get('/show/{id}', [SaleController::class, 'showAPI'])->name('sales.show');
+        Route::post('/update/{id}', [SaleController::class, 'updateAPI'])->name('sales.update');
+        Route::get('/delete/{id}', [SaleController::class, 'destroyAPI'])->name('sales.delete');
     });
 
     Route::group(['prefix' => 'purchases'], function(){
-        // Route::get('/list', [PurchaseController::class, 'index'])->name('purchases.list');
-        // Route::get('/create', [PurchaseController::class, 'create'])->name('purchases.create');
+        Route::get('/list', [PurchaseController::class, 'indexAPI'])->name('purchases.list');
         Route::post('/store', [PurchaseController::class, 'storeAPI'])->name('purchases.store');
-        // Route::get('/show/{id}', [PurchaseController::class, 'show'])->name('purchases.show');
-        // Route::get('/edit/{id}', [PurchaseController::class, 'edit'])->name('purchases.edit');
-        // Route::post('/update/{id}', [PurchaseController::class, 'edit'])->name('purchases.update');
-        // Route::get('/delete/{id}', [PurchaseController::class, 'destroy'])->name('purchases.delete');
+        Route::get('/show/{id}', [PurchaseController::class, 'showAPI'])->name('purchases.show');
+        Route::post('/update/{id}', [PurchaseController::class, 'updateAPI'])->name('purchases.update');
+        Route::get('/delete/{id}', [PurchaseController::class, 'destroyAPI'])->name('purchases.delete');
     });
 
     Route::group(['prefix' => 'articles'], function(){
-        // Route::get('/list', [ArticleController::class, 'index'])->name('articles.list');
-        // Route::get('/create', [ArticleController::class, 'create'])->name('articles.create');
+        Route::get('/list', [ArticleController::class, 'indexAPI'])->name('articles.list');
         Route::post('/store', [ArticleController::class, 'storeAPI'])->name('articles.store');
-        // Route::get('/show/{id}', [ArticleController::class, 'show'])->name('articles.show');
-        // Route::get('/edit/{id}', [ArticleController::class, 'edit'])->name('articles.edit');
-        // Route::post('/update/{id}', [ArticleController::class, 'edit'])->name('articles.update');
-        // Route::get('/delete/{id}', [ArticleController::class, 'destroy'])->name('articles.delete');
+        Route::get('/show/{id}', [ArticleController::class, 'showAPI'])->name('articles.show');
+        Route::post('/update/{id}', [ArticleController::class, 'updateAPI'])->name('articles.update');
+        Route::get('/delete/{id}', [ArticleController::class, 'destroyAPI'])->name('articles.delete');
     });
 
     Route::group(['prefix' => 'clients'], function(){
-        // Route::get('/list', [ClientController::class, 'index'])->name('clients.list');
-        // Route::get('/create', [ClientController::class, 'create'])->name('clients.create');
+        Route::get('/list', [ClientController::class, 'indexAPI'])->name('clients.list');
         Route::post('/store', [ClientController::class, 'storeAPI'])->name('clients.store');
-        // Route::get('/show/{id}', [ClientController::class, 'show'])->name('clients.show');
-        // Route::get('/edit/{id}', [ClientController::class, 'edit'])->name('clients.edit');
-        // Route::post('/update/{id}', [ClientController::class, 'edit'])->name('clients.update');
-        // Route::get('/delete/{id}', [ClientController::class, 'destroy'])->name('clients.delete');
+        Route::get('/show/{id}', [ClientController::class, 'showAPI'])->name('clients.show');
+        Route::post('/update/{id}', [ClientController::class, 'updateAPI'])->name('clients.update');
+        Route::get('/delete/{id}', [ClientController::class, 'destroyAPI'])->name('clients.delete');
     });
 });
 
