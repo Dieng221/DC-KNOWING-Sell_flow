f380ffee2c37ae1306ed5873d7318f355a1e0e2f
Mrsig11 <sis.konan07@gmail.com>
2025-01-09 08:39:06 +0000

    MAJ

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -148,7 +148,7 @@ class ArticleController extends Controller
                 'prix_vente' => ['numeric'],
             ]);
 
-            dd($validatedData);
+            // dd($validatedData);
             // Mettre à jour les données de l'achat
             $article->update($validatedData);
 
